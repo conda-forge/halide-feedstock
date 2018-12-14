da3481b04f534c0d3c0c439baf0e7c16b1e4f280
Min RK <benjaminrk@gmail.com>
2018-12-14 16:12:51 +0100

    avoid under-linking with dead_strip_dylibs

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,8 @@ export CLANG=$PREFIX/bin/clang
 
 # need to look in $PREFIX for zlib
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+# avoid under-linking with dead_strip_dylibs
+export LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
 
 make -j${CPU_COUNT}
 make install PREFIX=$PREFIX
