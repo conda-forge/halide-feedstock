3310e47379bb8dfffa6e9d39155a92a85a700d41
Isuru Fernando <ifernando@quansight.com>
2024-08-14 13:44:11 +0000

    Update to 18.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,20 @@
-if [[ $(uname) == "Darwin" && "$cxx_compiler" == "toolchain_cxx" ]]; then
-    # ensure toolchain uses host compiler on darwin
-    export CXX=/usr/bin/c++
-    export CC=/usr/bin/cc
-fi
-
-# avoid linking to libLLVM in build prefix
-rm -vf "$BUILD_PREFIX"/lib/libLLVM*.a
-
 export LLVM_CONFIG=$PREFIX/bin/llvm-config
 export CLANG=$PREFIX/bin/clang
 
-# need to look in $PREFIX for zlib
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 # avoid under-linking with dead_strip_dylibs
 export LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
 
+mkdir -p build
+cd build
+
+cmake ${CMAKE_ARGS} \
+  -DHalide_SHARED_LIBS=YES \
+  -DWITH_PYTHON_BINDINGS=NO \
+  ..
+
 make -j${CPU_COUNT}
-make install PREFIX=$PREFIX
+make install
+
+# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+#   ctest -j${CPU_COUNT}
+# fi
