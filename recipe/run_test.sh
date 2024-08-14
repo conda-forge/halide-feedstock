2c53a7ac3ed213399ca90acb87cb0d240eb457eb
Isuru Fernando <ifernando@quansight.com>
2024-08-14 13:54:46 +0000

    fix test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,10 +1,5 @@
-if [[ $(uname) == "Darwin" ]]; then
-    # ensure we test with host compiler on darwin
-    export CXX=/usr/bin/c++
-fi
-
-${CXX:-c++} -std=c++11 -I$PREFIX/include $RECIPE_DIR/test.cpp \
-    -Wl,-rpath,"$PREFIX/lib" -L$PREFIX/lib -lHalide -lz \
+${CXX} -std=c++11 ${CPPFLAGS} ${CXXFLAGS} test.cpp \
+    ${LDFLAGS} -lHalide -lz \
     -o test
 
 ./test
