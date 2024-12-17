f33577311da3aab23d69c603fbb755a4328d7fc2
Min RK <benjaminrk@gmail.com>
2024-12-17 10:26:39 +0100

    python: only build Python bindings
    don't rebuild libhalide again

--- recipe/install-python.sh
+++ recipe/install-python.sh
@@ -3,6 +3,10 @@ set -xeuo pipefail
 
 export CMAKE_PREFIX_PATH=$PREFIX
 
-$PYTHON -m pip install . -vv --no-deps
+# only build the Python bindings, not libHalide again
+$PYTHON -m pip install . -vv --no-deps -C cmake.source-dir=python_bindings
 
-rm -rvf $SP_DIR/halide/libHalide.so.*
+# fix installation of lib files inside Python package to $PREFIX/lib
+mv -v $SP_DIR/halide/lib/lib* $PREFIX/lib/
+mv -v $SP_DIR/halide/lib/cmake/* $PREFIX/lib/cmake/
+rm -rvf $SP_DIR/halide/lib
